ca5c775094802bd3a7bcd82a9a2677c1800e6291
Paul Franco <mail@paulfran.co>
2021-04-02 19:26:39 -0700

    Initial Commit

---
+++ SwiftUINFC/SwiftUINFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINFCApp.swift
+//  SwiftUINFC
+//
+//  Created by Paul Franco on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
